82a7d62f4e44924cb6b9f4a009da51e0b645b14f
joshmiller-dev <66091934+joshmiller-dev@users.noreply.github.com>
2020-06-29 03:01:30 -0400

    Add files via upload
    Changed some styles, added a solution button (albeit not the fastest/most optimal yet).  Made slightly more mobile friendly.

--- js/lights.js
+++ js/lights.js
@@ -1,11 +1,18 @@
-var seconds = 0;
-var minutes = 0;
-var firstClick = false;
-var timer;
-var matches1 = 0;
-var matches2 = 0;
-var solvable = false;
-var gridLayout = [];
+let seconds = 0;
+let minutes = 0;
+let moves = 0;
+let firstClick = false;
+var solving = false;
+let timer;
+let matches1 = 0;
+let matches2 = 0;
+let solvable = false;
+let gridLayout = [];
+let firstRow = [];
+let secondRow = [];
+let thirdRow = [];
+let fourthRow = [];
+const emptyRow = [0, 0, 0, 0, 0].toString();
 const quietPattern1 = [1, 0, 1, 0, 1, 
                        1, 0, 1, 0, 1, 
                        0, 0, 0, 0, 0, 
@@ -25,46 +32,48 @@ function init(){
 
     //randomizing layout
     randomizeLayout();
-
     //lights up board if solvable
     lightBoard();
-    //handles light logic
+    //handles button logic
     buttonLogic();
 
-
-    console.log(gridLayout);
-    console.log(quietPattern1);
-    console.log(quietPattern2);
-    console.log(matches1);
-    console.log(matches2);
-    console.log(solvable);
+    // console.log(gridLayout);
+    // console.log(quietPattern1);
+    // console.log(quietPattern2);
+    // console.log(matches1);
+    // console.log(matches2);
+    // console.log(solvable);
 }
 
+
 function randomizeLayout(){
+    //reset layout each time
     gridLayout = [];
 
     for(var i = 0; i < $(".square").length; i++){
         var randomNum = Math.random();
         if (randomNum < 0.5){
-            gridLayout.push(1);
-        }else{
             gridLayout.push(0);
+        }else{
+            gridLayout.push(1);
         }
     }
     testSolvability();       
 }
 
+
 function testSolvability(){
     matches1 = 0;
     matches2 = 0;
 
-    //testing generated pattern vs first quiet pattern
+    //testing generated pattern vs first quiet pattern (comparing number of lit lights)
     for (i = 0; i < gridLayout.length; i++){
         if(gridLayout[i] === 1 && quietPattern1[i] === 1){
             matches1 += 1;
         }
     }
 
+    //number of matches must be even, otherwise start over
     if (matches1 % 2 != 0){
         randomizeLayout();
     }else{
@@ -96,7 +105,6 @@ function lightBoard(){
 function buttonLogic(){
 
     $(".square").on("click", function(){
-
         //Only start timer on first click
         if (firstClick == false){
             startTimer();
@@ -104,16 +112,21 @@ function buttonLogic(){
         //set firstClick to true now
         firstClick = true;
 
+        //track moves
+        moves += 1;
+        $("#moves").text(moves);
+
+        //capture current firstClick square (by index)
+        let clickedLight = $(this).index();
         //Light or darken a square when clicked
         $(this).toggleClass('lit');
-        //capture current firstClick square
-        var clickedLight = $(this).index();
+
 
         //handling squares on RIGHT side
         if (clickedLight == 4 || clickedLight == 9 || clickedLight == 14 || clickedLight == 19){
             $(this).prev('.square').toggleClass('lit');
             $(".square").eq(clickedLight + 5).toggleClass('lit');
-            //handling negative number weirdness
+            //handling negative number wrapping
             if(clickedLight - 5 >= 0){
                 $(".square").eq(clickedLight - 5).toggleClass('lit');
             }
@@ -135,26 +148,45 @@ function buttonLogic(){
                 $(".square").eq(clickedLight - 5).toggleClass('lit');
             }
         }
+    
         //handling win condition
         if($(".square:not(.lit)").length === 25){
             setTimeout(function(){
                 gameWin();
             }, 500);
         }
+
+        //updating the gridlayout as we go
+        for (i = 0; i < gridLayout.length; i++){
+            if ($(".square").eq(i).hasClass("lit")){
+                gridLayout[i] = 1;
+            }else{
+                gridLayout[i] = 0;
+            }
+        }
+
+        firstRow = gridLayout.slice(0,5).toString();
+        secondRow = gridLayout.slice(5, 10).toString();
+        thirdRow = gridLayout.slice (10, 15).toString();
+        fourthRow = gridLayout.slice (15, 20).toString();
+    
+        if(solving == true){
+            solveGame();
+        }
+        //remove marked squares if following solution
+        if ($(this).hasClass("mark")){
+            $(this).removeClass('mark');
+        }
+
     });
 
+
     //Reset Button
-    $("#reset").on("click", function(){
+    $(".reset").on("click", function(){
         reset();
     });
 
-    // //Cheat Button
-    // $("#cheat").on("click", function(){
-    //     $(".square").removeClass('lit');
-    // });
-
 }
-
 
 function startTimer(x){
     //Handle the reset
@@ -181,21 +213,99 @@ function startTimer(x){
         }
         }, 1000);
     }
-
 }
+
 
 function gameWin(){
     clearInterval(timer);
     setTimeout(function(){
-        alert("You have won in: " + minutes + " minutes : " + seconds + " seconds!");
+        alert("You have won in: " + minutes + " minutes, " + seconds + " seconds using " + moves + " moves!");
     }, 500);
 }
 
 function reset(){
+    moves = 0;
+    $("#moves").text(moves);
     solvable = false;
     firstClick = false;
+    if ($(".square").hasClass("mark")){
+        $(".square").removeClass('mark');
+    }
+    solvable = false;
+    firstRow = [];
+    secondRow = [];
+    thirdRow = [];
+    fourthRow = [];
     randomizeLayout();
     lightBoard();
     //reset timer
     startTimer(1);
 }
+
+$(".solve").on("click", function(){
+    //remove previously marked squares
+    for (i = 0; i < gridLayout.length; i++){
+        $(".square").eq(i).removeClass("mark");
+    }
+    solving = true;
+    solveGame();
+});
+
+function solveGame(){
+    
+    //mark solution squares for first row
+    if (firstRow !== emptyRow){
+        for (var i = 0; i < 5; i++){
+            if ($(".square").eq(i).hasClass("lit")){
+                $(".square").eq(i + 5).addClass('mark');
+            }else{
+                $(".square").eq(i + 5).removeClass('mark');
+            }
+        }
+    }
+
+    //if first row is fully unlit, solve second row, and so on...
+    if (firstRow == emptyRow){
+        for (var i = 5; i < 10; i++){
+            if ($(".square").eq(i).hasClass("lit")){
+                $(".square").eq(i + 5).addClass('mark');
+            }else{
+                $(".square").eq(i + 5).removeClass('mark');
+            }
+        }       
+    }
+    if (secondRow == emptyRow){
+        for (var i = 10; i < 15; i++){
+            if ($(".square").eq(i).hasClass("lit")){
+                $(".square").eq(i + 5).addClass('mark');
+            }else{
+                $(".square").eq(i + 5).removeClass('mark');
+            }
+        }      
+    }
+    if (thirdRow == emptyRow){
+        for (var i = 15; i < 20; i++){
+            if ($(".square").eq(i).hasClass("lit")){
+                $(".square").eq(i + 5).addClass('mark');
+            }else{
+                $(".square").eq(i + 5).removeClass('mark');
+            }
+        }
+    }
+    if (firstRow == emptyRow && secondRow == emptyRow && thirdRow == emptyRow && fourthRow == emptyRow){
+        //final steps
+        //if light A5 is on, mark D1 and E1
+        //if light B5 is on, mark B1 and E1
+        //if light C5 is on, mark D1
+        if ($(".square").eq(20).hasClass("lit")){
+            $(".square").eq(3).addClass('mark');
+            $(".square").eq(4).addClass('mark');
+        }else if ($(".square").eq(21).hasClass("lit")){
+            $(".square").eq(1).addClass('mark');
+            $(".square").eq(4).addClass('mark');
+        }else if ($(".square").eq(22).hasClass("lit")){
+            $(".square").eq(3).addClass('mark');
+        }  
+    }
+}
+
